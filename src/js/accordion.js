f7688d7dd81df8fb354243ba32324c8824ebcfaf
Lisakova Anastasiia <a.lisakova@osla.ru>
2018-12-28 13:54:45 +0300

    copy berloni

---
+++ src/js/accordion.js
@@ -0,0 +1,15 @@
+var accordion = document.querySelectorAll('.accordion');
+
+if (window.innerWidth < 600) {
+  for (var i = 0; i < accordion.length; i++) {
+    accordion[i].addEventListener('click', function() {
+      this.classList.toggle('active');
+      var panel = this.nextElementSibling;
+      if (panel.style.maxHeight) {
+        panel.style.maxHeight = null;
+      } else {
+        panel.style.maxHeight = panel.scrollHeight + 'px';
+      }
+    });
+  }
+}
